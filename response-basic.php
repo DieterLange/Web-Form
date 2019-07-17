b4e10993e9b242b4dd98ab56f1ef8987dee117e8
DieterLange <51689600+DieterLange@users.noreply.github.com>
2019-07-18 09:20:04 +1000

    Fixed Datbase

--- response-basic.php
+++ response-basic.php
@@ -11,57 +11,59 @@
                     border: clear; border-radius: 20px; background-color: white; width: 25%;">
         <?php
             $name = $_POST["firstname"] ." ". $_POST["lastname"];
-            $phpgender = $_POST["gender"];
-            $phpage = $_POST["age"];
-            $phpnews = $_POST["news"];
+            $gender = $_POST["gender"];
+            $age = $_POST["age"];
+            $news = $_POST["news"];
             
-            $phpbuzzfeed = $_POST["buzzfeed"];
-            if ($phpbuzzfeed == "yes") { 
+            $buzzfeed = $_POST["buzzfeed"];
+            if ($buzzfeed == "yes") { 
             exit('ERROR');
             }
  
-            if ($phpgender == "male") {
+            if ($gender == "male") {
             echo "<div style='color: blue';> Welcome! $name <br> <br> </div>";
             }
             
-            elseif ($phpgender == "female") {
+            elseif ($gender == "female") {
             echo "<div style='color: pink';> Welcome! $name <br> <br> </div>";   
             }
             
-            elseif ($phpgender == "other") {
+            elseif ($gender == "other") {
             echo "<div style='color: red';> Welcome! $name <br> <br> </div>";   
             }
             
-            echo "Age: $phpage <br> <br>";
+            echo "Age: $age <br> <br>";
 
-            if ($phpnews == "yes") { 
-            echo "You will recive news letters";
+            if ($news == "yes") { 
+            echo "You will recive news letters <br> <br>";
             }
             
         ?>
         
         <?php
+        $firstname = $_POST["firstname"];
+        $lastname = $_POST["lastname"];
+        $gender = $_POST["gender"];
+        $age = $_POST["age"];
+        $newsletter = $_POST["news"];
+        //$buzzfeed = $_POST["buzzfeed"];
+        $sql = "INSERT INTO users (firstname, lastname, gender, age, newsletter) VALUES ('$firstname', '$lastname', '$gender', '$age', '$newsletter')";
+
         $servername = "localhost";
         $username = "root";
         $password = "root";
         $dbname = "signup";
         $conn = new mysqli($servername, $username, $password, $dbname);
         if ($conn->connect_error)   {
-            die("Connection failed: " . $conn->connect_error);
+        die("Connection failed: " . $conn->connect_error);
         }
 
-        $firstname = $_GET["firstname"];
-        $lastname = $_GET["lastname"];
-        $gender = $_GET["gender"];
-        $age = $_GET["age"];
-        $newsletter = $_GET["news"];
-        $buzzfeed = $_GET["buzzfeed"];
-        $sql = "INSERT INTO users (firstname, lastname, gender, age, news, buzzfeed) VALUES ('$firstname', '$lastname', '$gender', '$age', '$newsletter', '$buzzfeed')";
+        
 
         if ($conn->query($sql) === TRUE) {
-            echo "New record created succesfully"
+        echo "New record created succesfully";
         } else {
-            echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Error: " . $sql . "<br>" . $conn->error;
         }
         $conn->close();
         ?>
