b42343ea16600cb7cb9de29f59a7453b9c27107d
DieterLange <51689600+DieterLange@users.noreply.github.com>
2019-07-18 08:39:38 +1000

    Data Base Set Up

--- response-basic.php
+++ response-basic.php
@@ -39,6 +39,32 @@
             }
             
         ?>
+        
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "signup";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error)   {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $firstname = $_GET["firstname"];
+        $lastname = $_GET["lastname"];
+        $gender = $_GET["gender"];
+        $age = $_GET["age"];
+        $newsletter = $_GET["news"];
+        $buzzfeed = $_GET["buzzfeed"];
+        $sql = "INSERT INTO users (firstname, lastname, gender, age, news, buzzfeed) VALUES ('$firstname', '$lastname', '$gender', '$age', '$newsletter', '$buzzfeed')";
+
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created succesfully"
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        $conn->close();
+        ?>
     </div>
 </body>
 </html>
